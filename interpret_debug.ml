1f7c49a10f34849aafad6e7b2cfd7b1877e96b75
Jakub Wilk <jwilk@jwilk.net>
2013-01-25 17:37:08 +0100

    Update my e-mail address.

--- interpret_debug.ml
+++ interpret_debug.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2006, 2008 Jakub Wilk <ubanus@users.sf.net>
+ * Copyright (c) 2006, 2008 Jakub Wilk <jwilk@jwilk.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
